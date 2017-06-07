089849e92ce7d168a9ca28bd7334b742a49912c3
Mariia Suvalova <mariia_suvalova@epam.com>
2017-06-07 17:30:50 +0300

    Moved projects

---
+++ Module2_task2/DAC.ShipmentCompany/DAC.Dapper/Repositories/RouteRepositoryDapper.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using DapperExtensions;
+using DAC.Dapper.Interfaces;
+using DAC.Dapper.Models;
+
+namespace DAC.Dapper.Repositories
+{
+    public class RouteRepositoryDapper: IRoutesRepository
+    {
+        private readonly string _connectionString;
+
+        public RouteRepositoryDapper()
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["CDPDatabase"].ToString();
+        }
+
+        public IEnumerable<RouteOfCargo> GetAll()
+        {
+            IEnumerable<RouteOfCargo> list;
+            using (var cn = new SqlConnection(_connectionString))
+            {
+                cn.Open();
+                list = cn.GetList<RouteOfCargo>();
+                cn.Close();
+            }
+            return list;
+        }
+
+        public RouteOfCargo GetById(int id)
+        {
+            RouteOfCargo route;
+            using (var cn = new SqlConnection(_connectionString))
+            {
+                cn.Open();
+                route = cn.Get<RouteOfCargo>(id);
+                cn.Close();
+            }
+            return route;
+        }
+
+        public void Create(RouteOfCargo item)
+        {
+            using (var cn = new SqlConnection(_connectionString))
+            {
+                cn.Open();
+                cn.Insert(item);
+                cn.Close();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var cn = new SqlConnection(_connectionString))
+            {
+                cn.Open();
+                var item = cn.Get<RouteOfCargo>(id);
+                cn.Delete(item);
+                cn.Close();
+            }
+        }
+
+        public void Update(RouteOfCargo item)
+        {
+            using (var cn = new SqlConnection(_connectionString))
+            {
+                cn.Open();
+                cn.Update(item);
+                cn.Close();
+            }
+        }
+    }
+}
---
+++ Module2_task2/DAC.ShipmentCompany/DAC.EF/Repositories/RouteRepositoryEF.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using DAC.EF.Interfaces;
+
+namespace DAC.EF.Repositories
+{
+    public class RouteRepositoryEf : IRoutesRepositoryEf, IDisposable
+    {
+        private  ShipmentContext _context;
+
+        public RouteRepositoryEf(ShipmentContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<RouteOfCargo> GetAll()
+        {
+            return _context.RouteOfCargoes.ToList();
+        }
+
+        public RouteOfCargo GetById(int id)
+        {
+            return _context.RouteOfCargoes.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Create(RouteOfCargo item)
+        {
+            _context.RouteOfCargoes.Add(item);
+        }
+
+        public void Delete(int id)
+        {
+            var item = _context.RouteOfCargoes.Find(id);
+            if (item == null) return;
+            _context.RouteOfCargoes.Remove(item);
+        }
+
+        public void Update(RouteOfCargo item)
+        {
+            _context.Entry(item).State = EntityState.Modified;
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            if (_context != null)
+            {
+                _context.Dispose();
+                _context = null;
+            }
+        }
+    }
+}
